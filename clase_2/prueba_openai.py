ff8742a6b1f2d6fc6b2c6aa95baf4176fe3b4a01
emiperez997 <emi.perez997@gmail.com>
2025-01-29 12:11:07 -0300

    [ADD] Clase 2

---
+++ clase_2/prueba_openai.py
@@ -0,0 +1,13 @@
+from env import API_KEY
+from openai import OpenAI
+
+client = OpenAI()
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    store=True,
+    messages=[
+        {"role": "user", "content": "write a haiku about ai"}
+    ]
+)
+
+print(completion.choices[0].message.content)
